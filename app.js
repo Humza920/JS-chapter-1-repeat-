28b259aac859e62694fc1fbcf748a979bbd8eac8
Humza920 <humza.rehman.779@gmail.com>
2024-04-03 13:55:51 -0700

    DONE

---
+++ app.js
@@ -0,0 +1,20 @@
+// CHAPTER 1
+
+// QUESTION 1
+// alert("HELLO USER!");
+
+// QUESTION 2
+// alert("ERROR! Please enter a valid password");
+
+// QUESTION 3
+// alert("WELCOME! to JS Land \n Happy Coding");
+
+// QUESTION 4
+// alert("Welcome to JS-Land");
+// alert("Happy Coding");
+
+// QUESTION 5
+// It's run through browser's developer console;]
+
+// QUESTION 6
+// ISS MAI JAVASCRIPT KO INTERNALY CHALA KAR CHECKK KARNA HAI HTML KI FILE MAI
